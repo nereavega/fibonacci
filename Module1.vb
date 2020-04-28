b673c9143d900e0e5f153e950c1dfed2763a919e
Nerea Vega <ikayt@plaiaundi.net>
2020-04-28 10:58:04 +0200

    Agregar archivos de proyecto.

---
+++ Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Dim limite As Integer
+        Dim secuencia As String = " |"
+
+        Console.WriteLine("Introduce el límite de la secuencia Fibonacci")
+        limite = Console.ReadLine()
+
+        For i As Integer = 1 To limite
+            secuencia += Fibonacci(i) + " |"
+        Next i
+
+        Console.Write("Secuencia: " + secuencia)
+    End Sub
+
+    Private Function Fibonacci(i As Integer) As String
+        If (i = 0 Or i = 1) Then
+            Return i
+        Else
+            Return Fibonacci(i - 1) + Fibonacci(i - 2)
+        End If
+    End Function
+
+End Module
